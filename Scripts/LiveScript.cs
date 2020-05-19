c6d6178eba1b7559160a724f37c10d175bb03a17
Lior Elisberg <liorelisberg@gmail.com>
2020-05-19 14:02:39 +0300

    added c# scripts folder

---
+++ Scripts/LiveScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DisallowMultipleComponent]
+public class LiveScript : MonoBehaviour
+{
+    public GameObject rocket1, rocket2, rocket3;
+    public static int health = 3;
+
+    void Start()
+    {
+        rocket1.gameObject.SetActive(true);
+        rocket2.gameObject.SetActive(true);
+        rocket3.gameObject.SetActive(true);
+    }
+    void Update()
+    {
+        DisplayCurrentLives();
+    }
+    private void DisplayCurrentLives()
+    {
+        switch (health)
+        {
+            case 3:
+                rocket1.gameObject.SetActive(true);
+                rocket2.gameObject.SetActive(true);
+                rocket3.gameObject.SetActive(true);
+                break;
+            case 2:
+                rocket1.gameObject.SetActive(true);
+                rocket2.gameObject.SetActive(true);
+                rocket3.gameObject.SetActive(false);
+                break;
+            case 1:
+                rocket1.gameObject.SetActive(true);
+                rocket2.gameObject.SetActive(false);
+                rocket3.gameObject.SetActive(false);
+                break;
+            case 0:
+                rocket1.gameObject.SetActive(false);
+                rocket2.gameObject.SetActive(false);
+                rocket3.gameObject.SetActive(false);
+                // Invoke("LoadEndScene", 2f);
+                break;
+            default:
+                print("illegal number of lifes");
+                break;
+
+        }
+    }
+    void LoadEndScene()
+    {
+        health = 3;
+        SceneManager.LoadScene(5);
+    }
+}
